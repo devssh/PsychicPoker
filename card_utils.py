3df12149cf3368394c161f0e9453c7377eaddd0e
devssh <devasood@gmail.com>
2018-03-01 21:01:36 +0530

    Add proper exception to be raised on invalid input

--- card_utils.py
+++ card_utils.py
@@ -1,6 +1,8 @@
 card_order_dict = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "T": 10, "J": 11, "Q": 12, "K": 13,
                    "A": 14}
 
+suit_set = {'H', 'S', 'D', 'C'}
+
 
 def get_rank(some_card):
     return some_card[0]
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from card_utils import get_suit, get_rank, suit_set, card_order_dict
 from poker_predictor import calculate_best_outcome
 
 # since I am doing it in Python which is not strongly typed, I'll use functions instead of classes
@@ -7,7 +8,8 @@ from poker_predictor import calculate_best_outcome
 if __name__ == '__main__':
     count = int(input('Enter number of input lines/trials\n'))
     for _ in range(count):
-        data = input('Enter the input of 5 cards in hand and top 5 cards of deck\n').strip()
+        data = input('Enter the input of 5 cards in hand and top 5 cards of deck\n For example\n' +
+                     'TH JH QC QD QS QH KH AH 2S 6S\n\n').strip()
         # data = [
         #     'TH JH QC QD QS QH KH AH 2S 6S',
         #     '2H 2S 3H 3S 3C 2D 3D 6C 9C TH',
@@ -19,10 +21,17 @@ if __name__ == '__main__':
         #     '6C 9C 8C 2D 7C 2H TC 4C 9S AH',
         #     '3D 5S 2H QD TD 6S KH 9H AD QH'
         # ]
-        assert ' ' in data
-        assert len(data) == 29
+        if ' ' not in data:
+            raise ValueError('Invalid input format')
+        if len(data.split(' ')) != 10:
+            raise ValueError('Incorrect number of inputs, 10 inputs required')
         cards = data.split(' ')
-        assert len(cards) == 10
+        if any([len(card) != 2 for card in cards]):
+            raise ValueError('Bad format for cards, please check all cards are rank:suit format like 2H')
+        if any([get_suit(card) not in suit_set for card in cards]):
+            raise ValueError('Bad format for cards, invalid suit given in a card')
+        if any([get_rank(card) not in card_order_dict.keys() for card in cards]):
+            raise ValueError('Bad format for cards, invalid rank given in a card')
         card_len = 5
         hand = cards[:card_len]
         deck = cards[card_len:]
